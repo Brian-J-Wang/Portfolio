76977f9abe9ab68c740c699770711cda4000c5fc
Brian Wang <brianwang7102@gmail.com>
2026-06-21 21:08:39 -0400

    docker: added dockerfile

--- apps/astro-server/astro.config.mjs
+++ apps/astro-server/astro.config.mjs
@@ -2,9 +2,19 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
+      resolve: {
+          //@ts-ignore
+          tsconfigPaths: true,
+      },
 	},
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
